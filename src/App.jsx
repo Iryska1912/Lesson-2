7e382a6f1774c4a79082fef07414984579d6854e
Iryska1912 <Iryska.ua.pl@gmail.com>
2025-04-02 10:45:37 +0200

    klasa 1a

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,42 @@ import "./App.css";
 import bd from "../bd.json";
 
 function App() {
-const a1=32, g=5, n_k=5;
+const aa=5, bb=3, m_komp=140, fhghj_hvhdrjh=7;
+let cc,dd=8,ee;
+cc=aa*bb;
+ee=dd+aa-cc*17+2*aa*aa;
+console.log(cc,ee,'hej');
+let wyn1=m_komp*1;
+let wyn2=m_komp*2;
+let wyn3=m_komp*3;
+let wyn4=m_komp*4;
+let wyn5=m_komp*5;
+let wyn6=m_komp*6;
+let wyn7=m_komp*7;
+let wyn8=m_komp*8;
+let wyn9=m_komp*9;
+let wyn10=m_komp*10;
+
+
+ 
+
+
+
+
+  const a1=32, g=5, n_k=5;
 let a2,a3=1, s1,s2,s3, sum2;
 sum2=a1+g;
-console.log(a1,sum2);
+
 s1=n_k*1;
 s2=n_k*2;
 s3=n_k*3;
-
-
-
-
-
-
-
-
-
 
 const komp=3, a=1, b=100;
 let  x, y, z, sum, w1,w2,w3,w4,w5,w6,w7,w8,w9;
 x=2, y=4;
 z=x+y;
 sum=2*x+3*y;
-console.log(a,z, sum);
+
 w1=komp*1;
 w2=komp*2;
 w3=komp*3;
@@ -96,6 +109,32 @@ w9=komp*9;
 
   return (
     <>
+    <div className="ddddd">
+    <p className="ppppp">mój komp numer {m_komp}</p>
+    </div>
+    <br />
+    <div className="ddddd2">
+<p>Tablica mnożenia</p>
+<p>{m_komp}*1={wyn1}</p>
+<p>{m_komp}*2={wyn2}</p>
+<p>{m_komp}*3={wyn3}</p>
+<p>{m_komp}*4={wyn4}</p>
+<p>{m_komp}*5={wyn5}</p>
+<p>{m_komp}*6={wyn6}</p>
+<p>{m_komp}*7={wyn7}</p>
+<p>{m_komp}*8={wyn8}</p>
+<p>{m_komp}*9={wyn9}</p>
+<p>{m_komp}*10={wyn10}</p>
+    </div>
+
+
+
+
+
+
+
+
+
     <p className="ppp3">moj komp - {n_k}</p>
     <p>{n_k}*1={s1}</p>
     <p>{n_k}*2={s2}</p>
